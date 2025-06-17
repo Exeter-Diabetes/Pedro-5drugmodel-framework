3efca5de5d20ef6dc4abb2a64dabf34e6127fd1a
Pedro Cardoso <104718776+PM-Cardoso@users.noreply.github.com>
2025-06-17 11:26:04 +0100

    Renamed file and updated comments.

--- 02.CPRD/02.table_characteristics.R
+++ 02.CPRD/02.table_characteristics.R
@@ -1,60 +1,37 @@
+
+# Initial set-up ###########################################
+
 # load libraries
 library(tidyverse)
 library(aurum)
-library(tableone)
+library(rms)
 
 # set up aurum
 cprd = CPRDData$new(cprdEnv = "diabetes-jun2024", cprdConf = "~/.aurum.yaml")
 analysis = cprd$analysis("pedro_mm")
 
-# set up functions
+## functions ----
 is.integer64 <- function(x){
   class(x)=="integer64"
 }
-# set up dataset
-analysis_pre_2020_raw <- analysis_pre_2020_raw %>%
-  analysis$cached("analysis_pre_2020") %>%
-  collect() %>%
-  mutate(patid=as.character(patid)) %>%
-  mutate_if(is.integer64, as.integer)
 
+# set up dataset
 analysis_post_2020_raw <- analysis_post_2020_raw %>%
   analysis$cached("analysis_post_2020") %>%
   collect() %>%
   mutate(patid=as.character(patid)) %>%
   mutate_if(is.integer64, as.integer)
 
+analysis_pre_2020_raw <- analysis_pre_2020_raw %>%
+  analysis$cached("analysis_pre_2020") %>%
+  collect() %>%
+  mutate(patid=as.character(patid)) %>%
+  mutate_if(is.integer64, as.integer)
 
-#:------------------------------------------------------
-# Pre-processing datasets
-#:-- Pre 2020-10-14
-analysis_pre_2020 <- analysis_pre_2020_raw %>%
-  mutate(
-    pated = paste(patid, dstartdate, drug_class, sep = "."),
-    sex = factor(gender, levels = c(1, 2), labels = c("Male", "Female")),
-    agetx = dstartdate_age,
-    ethnicity = ifelse(is.na(ethnicity_5cat), 5, ethnicity_5cat),
-    ethnicity = factor(ethnicity, levels = c(0:5), labels = c("White", "South Asian", "Black", "Other", "Mixed", "Missing")),
-    
-    smoke = ifelse(is.na(smoking_cat), "Not recorded", smoking_cat),
-    smoke = factor(smoke, levels = c("Non-smoker", "Active smoker", "Ex-smoker", "Not recorded")),
-    imd5 = ifelse(is.na(imd_decile), 5, imd_decile),
-    imd5 = factor(ceiling(imd5/2), levels = c(1, 2, 3, 4, 5), labels = c("1 (least)", "2", "3", "4", "5 (most)")),
-    
-    ncurrtx = MFN + SGLT2 + GLP1 + DPP4 + TZD + SU,
-    ncurrtx = ifelse(ncurrtx > 4, 4, ncurrtx),
-    ncurrtx = factor(ncurrtx, levels = c(1:4), labels = c("1", "2", "3", "4+")),
-    drugline = ifelse(drugline_all > 5, 5, drugline_all),
-    drugline = factor(drugline, levels = c(2:5), labels = c("2", "3", "4", "5+")),
-    drugclass = drug_class
-  ) %>%
-  group_by(pated) %>% 
-  mutate(row = 1:n()) %>% 
-  ungroup() %>% 
-  filter(row == 1) %>%
-  select(-row)
 
-#:-- Post 2020-10-14
+# Pre-processing datasets ########################################
+
+## Post 2020-10-14 ----
 analysis_post_2020 <- analysis_post_2020_raw %>%
   mutate(
     pated = paste(patid, dstartdate, drug_class, sep = "."),
@@ -75,17 +52,66 @@ analysis_post_2020 <- analysis_post_2020_raw %>%
     drugline = factor(drugline, levels = c(2:5), labels = c("2", "3", "4", "5+")),
     drugclass = drug_class
   ) %>%
-  group_by(pated) %>% 
-  mutate(row = 1:n()) %>% 
-  ungroup() %>% 
+  group_by(pated) %>%
+  mutate(row = 1:n()) %>%
+  ungroup() %>%
   filter(row == 1) %>%
-  select(-row)
+  select(-row) %>%
+  select(all_of(c(
+    "pated", "agetx", "sex", "t2dmduration", "ethnicity",
+    "drug_substance", "drug_class",
+    "imd5", "smoke",
+    "prebmi", "prehba1c", "preegfr", "pretotalcholesterol", "prehdl", "prealt",
+    "drugline", "ncurrtx", "hba1cmonth",
+    "posthba1cfinal"
+  )
+  )) %>%
+  rename("drugclass" = "drug_class") %>%
+  as.data.frame()
+
+
+## Pre 2020-10-14 ----
+analysis_pre_2020 <- analysis_pre_2020_raw %>%
+  mutate(
+    pated = paste(patid, dstartdate, drug_class, sep = "."),
+    sex = factor(gender, levels = c(1, 2), labels = c("Male", "Female")),
+    agetx = dstartdate_age,
+    ethnicity = ifelse(is.na(ethnicity_5cat), 5, ethnicity_5cat),
+    ethnicity = factor(ethnicity, levels = c(0:5), labels = c("White", "South Asian", "Black", "Other", "Mixed", "Missing")),
+    
+    smoke = ifelse(is.na(smoking_cat), "Not recorded", smoking_cat),
+    smoke = factor(smoke, levels = c("Non-smoker", "Active smoker", "Ex-smoker", "Not recorded")),
+    imd5 = ifelse(is.na(imd_decile), 5, imd_decile),
+    imd5 = factor(ceiling(imd5/2), levels = c(1, 2, 3, 4, 5), labels = c("1 (least)", "2", "3", "4", "5 (most)")),
+    
+    ncurrtx = MFN + SGLT2 + GLP1 + DPP4 + TZD + SU,
+    ncurrtx = ifelse(ncurrtx > 4, 4, ncurrtx),
+    ncurrtx = factor(ncurrtx, levels = c(1:4), labels = c("1", "2", "3", "4+")),
+    drugline = ifelse(drugline_all > 5, 5, drugline_all),
+    drugline = factor(drugline, levels = c(2:5), labels = c("2", "3", "4", "5+")),
+    drugclass = drug_class
+  ) %>%
+  group_by(pated) %>%
+  mutate(row = 1:n()) %>%
+  ungroup() %>%
+  filter(row == 1) %>%
+  select(-row) %>%
+  select(all_of(c(
+    "pated", "agetx", "sex", "t2dmduration", "ethnicity",
+    "drug_substance", "drug_class",
+    "imd5", "smoke",
+    "prebmi", "prehba1c", "preegfr", "pretotalcholesterol", "prehdl", "prealt",
+    "drugline", "ncurrtx", "hba1cmonth",
+    "posthba1cfinal"
+  )
+  )) %>%
+  rename("drugclass" = "drug_class") %>%
+  as.data.frame()
 
 
 
-#:-----------------------------------------------------------------------------
-# Table of characteristics for each dataset
-#:-----------------------------------------------------------------------------
+
+# Table of characteristics ----
 
 vars <- c(
   "agetx", "sex", "t2dmduration", "ethnicity", 
@@ -102,7 +128,7 @@ cat_vars <- c(
 )
 
 
-#:-- Pre 2020-10-14
+## Pre 2020-10-14 ----
 table_characteristics_pre2020 <- CreateTableOne(
   vars = vars,
   factorVars = cat_vars,
@@ -118,7 +144,7 @@ table_characteristics_pre2020_print <- print(table_characteristics_pre2020, exac
 write.csv(table_characteristics_pre2020_print, file = "Outputs/CPRD/01.02.characteristics_pre2020.csv")
 
 
-#:-- Post 2020-10-14
+## Post 2020-10-14 ----
 analysis_post_2020 <- analysis_post_2020 %>%
   mutate(
     prebmi_missing = ifelse(is.na(prebmi), "Yes", "No"),
